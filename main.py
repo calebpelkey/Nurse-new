32d18d41c29f54a1ecc64eab2d3d0e42887f3ded
calebpelkey <152338622+calebpelkey@users.noreply.github.com>
2023-12-05 18:20:47 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import os
-from time import sleep
-from packaging import version
 from PyPDF2 import PdfReader
 import docx
 import functions
@@ -47,37 +45,60 @@ def get_openai_chat_response(api_key, model, prompt, max_tokens):
 def main():
     st.title("Nursing Resume Assistant")
 
-    # Load the OpenAI API key from Streamlit secrets
-    openai_api_key = st.secrets["OPENAI_API_KEY"]
-    model = "gpt-4-1106-preview"  # Define the model here
+    # Initialize session state for history and user input if not already present
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
+    if 'user_input' not in st.session_state:
+        st.session_state['user_input'] = ""
 
-    # Document upload and text extraction
+    # Load the OpenAI API key from an environment variable
+    openai_api_key = os.getenv('OPENAI_API_KEY')
+    if openai_api_key is None:
+        st.error("OpenAI API key not found. Please set the OPENAI_API_KEY environment variable.")
+        return
+    model = 'gpt-4-1106-preview'
+
+    # Display the conversation history
+    for i, (role, text) in enumerate(st.session_state['history']):
+        if role == 'user':
+            st.markdown(f"**User:**\n{text}", unsafe_allow_html=True)
+        else:
+            st.markdown(f"**AI:**\n{text}", unsafe_allow_html=True)
+        if i < len(st.session_state['history']) - 1:
+            st.text("")  # Add space after each entry except the last
+
+    # Text input for user's query
+    user_input = st.text_input("Enter your query related to the uploaded document:", value=st.session_state.user_input)
+
+    # When the user sends a query
+    if st.button("Get AI Response"):
+        # Append user query to history
+        st.session_state['history'].append(('user', user_input))
+
+        # Call the function that communicates with OpenAI API
+        response = handle_query(user_input, st.session_state.get('document_text', ""), openai_api_key, model)
+
+        # Check if the response is valid and then update the history
+        if 'choices' in response and response['choices']:
+            ai_response = response['choices'][0]['message']['content']
+            st.session_state['history'].append(('ai', ai_response))
+        else:
+            st.error("No response received from the AI.")
+
+        # Clear the input field
+        st.session_state.user_input = ""
+        st.experimental_rerun()
+
+    # The "Upload your document" section is now placed below the query input
     uploaded_file = st.file_uploader("Upload your document", type=["pdf", "docx", "txt"])
-    document_text = ""
     if uploaded_file is not None:
         # Process the file based on its type
         if uploaded_file.type == "application/pdf":
-            document_text = extract_text_from_pdf(io.BytesIO(uploaded_file.read()))
+            st.session_state['document_text'] = extract_text_from_pdf(io.BytesIO(uploaded_file.read()))
         elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-            document_text = extract_text_from_docx(io.BytesIO(uploaded_file.read()))
+            st.session_state['document_text'] = extract_text_from_docx(io.BytesIO(uploaded_file.read()))
         else:
-            document_text = uploaded_file.getvalue().decode("utf-8")
-
-    # User input for questions
-    user_input = st.text_input("Enter your query related to the uploaded document:")
-    if st.button("Get AI Response"):
-        try:
-            response = handle_query(user_input, document_text, openai_api_key, model)
-
-            # Display the response
-            if response.get('choices'):
-                last_message = response['choices'][0]['message']['content']
-                st.write("Response from OpenAI:", last_message)
-            else:
-                st.error("No response received from OpenAI.")
-
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
+            st.session_state['document_text'] = uploaded_file.getvalue().decode("utf-8")
 
 if __name__ == "__main__":
     main()
